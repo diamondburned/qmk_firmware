29025afcff2df93311faaa369bbcf59f0c79a452
diamondburned <diamond@libdb.so>
2024-05-31 00:31:04 -0700

    Add crkbd:asynchronousart

---
+++ keyboards/crkbd/keymaps/asynchronousart/json-to-c.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+convert() {
+	nlayers=$(jq -r '.layers | length' keymap.json)
+	layout=$(jq -r '.layout' keymap.json)
+
+	cat<<'EOF'
+#include QMK_KEYBOARD_H
+#include <stdio.h>
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+EOF
+	
+	for ((i = 0; i < nlayers; i++)); {
+		echo "[$i] = ${layout}("
+		jq -r ".layers[$i] | join(\", \")" keymap.json
+		echo "),"
+	}
+
+	cat<<'EOF'
+};
+EOF
+}
+
+fmt() {
+	command -v clang-format &> /dev/null && clang-format || cat
+}
+
+main() {
+	out="keymap.c"
+	[[ "$1" ]] && out="$1"
+	convert | fmt > "$out"
+}
+
+main "$@"
